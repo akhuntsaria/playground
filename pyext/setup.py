95ca2369ae81fafbcdab5cbae90c9a33a0d755ae
Akaki Khuntsaria <akhuntsaria@icloud.com>
2025-01-06 14:22:17 +0100

    idk

--- pyext/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-mat_module = Extension('mat', sources=['matmodule.cpp'])
-
-setup(name='mat',
-      version='0.0.1',
-      description='description',
-      ext_modules=[mat_module])
